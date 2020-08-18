2cf5f4087981b5512462f76c3d474c1108942834
Ankit Kumar <ph.official.ankit@gmail.com>
2020-08-18 21:21:07 +0530

    Comparison Operations

---
+++ Comparison_Ops.py
@@ -0,0 +1,18 @@
+print('Welcome to the comparison operations')
+
+print(2 == 2)
+print('hello' == 'bye')
+print('bye' == 'Bye')
+print(3 == 3.0)
+print(3 == '3')
+print(3 != 4)
+print(2 < 1)
+print(3 <= 4)
+print(4 >= 2)
+
+print(1 < 2 < 3)
+print((1 < 3) and (3 < 4))
+print('hi' == 'hi' and 2.0 > 3)
+print(1 == 3 or 2 < 3)
+print(not 1 == 3)
+print(not "hi" == 'hi')
