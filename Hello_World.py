d6d10093445febbea6c67ed6e1d1ea570ece147b
Ankit Kumar <ph.official.ankit@gmail.com>
2020-08-20 17:45:21 +0530

    Hello world

---
+++ Hello_World.py
@@ -0,0 +1,18 @@
+print('Hi, I am Ankit')
+print('This is the Hello World program')
+
+a = 10
+print(a)
+a = a + a
+print(a)
+print(type(a))
+a = 30.1
+print(a)
+print(type(a))
+
+print(True)
+print(type(False))
+print(1 > 2)
+print(3 == 3)
+
+print('The End')
