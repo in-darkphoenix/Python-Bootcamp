1a56209ecd337dfaf56a62836d8611b103bcf7f3
Ankit Kumar <ph.official.ankit@gmail.com>
2020-08-30 11:11:05 +0530

    Set operations

---
+++ Set_Ops.py
@@ -0,0 +1,14 @@
+print('Wlcome to set ops')
+
+s = {1, 2}
+print(s)
+print(type(s))
+s.add(3)
+print(s)
+s.add(4)
+print(s)
+s.add(3)
+print(s)
+
+l = [1, 1, 1, 1, 5, 5, 5, 6, 6, 7, 7, 7, 7, 7, 8]
+print(set(l))
