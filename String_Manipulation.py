9405cf78ac1e8dcf33658726036733a0c9ff9650
Ankit Kumar <ph.official.ankit@gmail.com>
2020-08-31 19:04:56 +0530

    String manipulations

---
+++ String_Manipulation.py
@@ -0,0 +1,46 @@
+print('same\tline')
+print("new\nline")
+print(len('Hi whats my length'))
+
+s = 'MISSISSIPPI'
+print(s[4])
+print(s[-2])
+print(s[3:])
+print(s[:5])
+print(s[2:7])
+print(s[::3])
+print(s[0:8:2])
+
+x = 'Hey! '
+y = "how you doing"
+print(x+y)
+a = '6'
+b = "9"
+print(a+b)
+
+ch = 'a'
+print(ch*10)
+
+x = 'elephant'
+print(x.upper())
+y = 'APPLE'
+print(y.lower())
+z = "Can you be more awkward!"
+print(z.split())
+print(z.split('e'))
+
+print('Welcome {}'.format('boii'))
+print('Why {} {} {}'.format('running', 'are', "you"))
+print('Why {1} {2} {0}'.format('running', 'are', "you"))
+print('Welcome {t} {m} {k}'.format(m="my", t='to', k="kingdom"))
+
+r = 200/89
+print(r)
+print('The precise result is {rs:1.5f}'.format(rs=r))
+
+p = 'Do'
+q = "love"
+print(f"{p} you {q} her")
+
+
+
